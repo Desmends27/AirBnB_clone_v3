1c650fd9855c185055479a99ab36e4d2d0bf2b6e
Desmends27 <desmondopokumends2004@gmail.com>
2024-05-01 20:38:11 +0000

    some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """ Index file of flask app """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route("/status")
 def status():
@@ -10,3 +11,16 @@ def status():
             "status": "OK"
             }
     return jsonify(stat)
+
+
+@app_views.route("/stats")
+def stats():
+    """ Returns number of each object by type """
+    classes = {"User": "users",
+                "Amenity": "amenities", "City": "cities",
+                "Place": "places", "Review": "reviews",
+                "State": "states"}
+    numbers = {}
+    for key, val in classes.items():
+        numbers[val] = storage.count(key)
+    return jsonify(numbers)

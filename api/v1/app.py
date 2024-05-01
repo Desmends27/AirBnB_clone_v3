18c34418fa01620edaedd635fc027723ad0d33ed
Desmends27 <desmondopokumends2004@gmail.com>
2024-05-01 20:43:13 +0000

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ flask application  instance created here"""
-from flask import Flask
+from flask import Flask, jsonify
 from flask import Blueprint
 from models import storage
 from api.v1.views import app_views
@@ -11,9 +11,17 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def storage_close(self):
+def storage_close(exception):
     """ Calls the storage.close method """
     storage.close()
+
+@app.errorhandler(404)
+def error404(e):
+    """ Prints a not found for incorrect url's """
+    error = {
+            "error" : "Not found"
+            }
+    return jsonify(error)
 
 
 if __name__ == "__main__":

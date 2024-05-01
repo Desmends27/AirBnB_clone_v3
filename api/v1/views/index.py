c4b6f2ecfa42e71c98b4bb347c10a1e0a8cf4d8b
Desmends27 <desmondopokumends2004@gmail.com>
2024-05-01 20:16:26 +0000

    index pep8 error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" Index file of flask app """
 from api.v1.views import app_views
 from flask import jsonify
 
 @app_views.route("/status")
 def status():
+    """ Status of the api """
     stat = {
             "status": "OK"
             }
